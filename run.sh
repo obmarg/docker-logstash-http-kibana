9d5832b7159b097741c6fa06e1711ea4d7902cdf
Graeme Coupar <grambo@grambo.me.uk>
2014-03-21 00:14:52 +0000

    Switched to elasticsearch_http logstash output
    The elasticsearch output is a bit picky about versions, so http seems
    like the way to go.

--- run.sh
+++ run.sh
@@ -5,17 +5,15 @@ SITE_CONF=/etc/nginx/sites-available/es-kibana
 
 ES_HOST=$ES_PORT_9200_TCP_ADDR
 ES_PORT=$ES_PORT_9200_TCP_PORT
-ES_TRANSPORT_HOST=$ES_PORT_9300_TCP_ADDR
-ES_TRANSPORT_PORT=$ES_PORT_9300_TCP_PORT
 
 # TODO: At the moment these will only work on first run.  Fix that.
 # Configure logstash
-sed "s/{{ES_HOST}}/$ES_TRANSPORT_HOST/" -ibac $LOGSTASH_CONF
-sed "s/{{ES_PORT}}/$ES_TRANSPORT_PORT/" -ibac2 $LOGSTASH_CONF
+sed "s/{{ES_HOST}}/$ES_HOST/" -i $LOGSTASH_CONF
+sed "s/{{ES_PORT}}/$ES_PORT/" -i $LOGSTASH_CONF
 
 # Configure nginx
-sed "s/{{ES_HOST}}/$ES_HOST/" -ibac $SITE_CONF
-sed "s/{{ES_PORT}}/$ES_PORT/" -ibac2 $SITE_CONF
-sed "s/{{MY_HOST}}/$MY_HOST/" -ibac3 $SITE_CONF
+sed "s/{{ES_HOST}}/$ES_HOST/" -i $SITE_CONF
+sed "s/{{ES_PORT}}/$ES_PORT/" -i $SITE_CONF
+sed "s/{{MY_HOST}}/$MY_HOST/" -i $SITE_CONF
 
 supervisord
